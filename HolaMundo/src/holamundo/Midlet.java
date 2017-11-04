892451817e9c1c46cca2e72eba8f27a5e63bc7ce
lcordon28 <33380638+lcordon28@users.noreply.github.com>
2017-11-04 16:03:38 -0500

    Add files via upload

---
+++ HolaMundo/src/holamundo/Midlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author personal
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
